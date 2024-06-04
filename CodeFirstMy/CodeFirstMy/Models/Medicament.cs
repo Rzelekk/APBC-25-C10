6e944ece29c22c40b8a12b55f70e2fd8ec7f8c51
Rzelek <55500426+Rzelekk@users.noreply.github.com>
2024-06-04 11:38:24 +0200

    add property

--- CodeFirstMy/CodeFirstMy/Models/Medicament.cs
+++ CodeFirstMy/CodeFirstMy/Models/Medicament.cs
@@ -1,6 +1,21 @@
-﻿namespace CodeFirstMy.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirstMy.Models;
 
 public class Medicament
 {
+    [Key]
+    public int IdMedicament { get; set; }
     
+    [MaxLength(100)] 
+    public string Name { get; set; }
+    
+    [MaxLength(100)] 
+    public string Description { get; set; }
+    
+    [MaxLength(100)] 
+    public string Type { get; set; }
+    
+    public ICollection<PrescriptionMedicament> PrescriptionMedicaments { get; set; } =
+        new HashSet<PrescriptionMedicament>();
 }
