73b0642bc8851b2159ada2a01ef47479ff5954d0
Rzelek <55500426+Rzelekk@users.noreply.github.com>
2024-06-04 11:43:15 +0200

    add properties

--- CodeFirstMy/CodeFirstMy/Data/ApplicationContext.cs
+++ CodeFirstMy/CodeFirstMy/Data/ApplicationContext.cs
@@ -1,8 +1,33 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CodeFirstMy.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodeFirstMy.Data;
 
 public class ApplicationContext : DbContext
 {
+    protected ApplicationContext()
+    {
+    }
+
+    public ApplicationContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Medicament> Medicaments { get; set; }
+    public DbSet<Prescription> Prescriptions { get; set; }
+    public DbSet<PrescriptionMedicament> PrescriptionMedicaments { get; set; }
     
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        // modelBuilder.Entity<Author>().HasData(new List<Author>()
+        // {
+        //     new() { Id = 1, FirstName = "John", LastName = "Doe"},
+        //     new() { Id = 2, FirstName = "Ann", LastName = "Smith"},
+        //     new() { Id = 3, FirstName = "Jack", LastName = "Taylor"}
+        // });
+    }
 }
